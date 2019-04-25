4bf662d15793a356d8d2ec91a704bdc4c5debff5
kpassaglia <kpassaglia@gmail.com>
2019-04-25 09:50:27 -0500

    beach loop added

--- Assets/js/weatherAPI.js
+++ Assets/js/weatherAPI.js
@@ -2,10 +2,29 @@
 var apiKEY = "5af5afe42af981e26b5ba776c7767b41";
 var lat = 41.881832;
 var lon = -87.623177;
+var beachLocations = [
+    {
+        beach: "Oak Street",
+        lat: 41.9030,
+        lon: 87.6228
+    },
+    {
+        beach: "North Ave",
+        lat: 41.9148,
+        lon: 87.6251
+    },
+    {
+        beach: "Montrose",
+        lat: 41.9663,
+        lon: 87.6372
+    }
+];
 
+var beachSelected = $("#beachSelected").val()
 
-$('#test').on('click', function () {
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid="+ apiKEY;
+$('#send').on('click', function () {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKEY;
     // var converterMath= ((K-273.15)*1.8)+32
     $.ajax({
         url: queryURL,
@@ -14,37 +33,38 @@ $('#test').on('click', function () {
         .then(function (response) {
             console.log(queryURL);
             console.log(response);
-           
-            
-            var tempK = response.list[18].main.temp;
+
+            var tempK = response.list[0].main.temp;
             var time = response.list[0].dt_txt;
-            console.log(tempK + "K");
-            console.log(time);
-            var tempF = ((tempK - 273.15)*1.8)+32;
-            console.log(tempF + "F");
-           
-        //     for (var i = 0; i < results.length; i++) {
-        //         var gifDiv = $("<div>");
-        //         var rating = results[i].rating;
-        //         console.log(rating);
+            var tempF = ((tempK - 273.15) * 1.8) + 32;
 
-        //         var p = $("<p>").text("Rating: " + rating);
+            for (var i = 0; i < 3; i++) {
+                time = response.list[i].dt_txt
+                tempK = response.list[i].main.temp
+                console.log("Time: " + time + " Temp: " + tempF)
+            };
+            // console.log(time)
+            //         var gifDiv = $("<div>");
+            //         var rating = results[i].rating;
+            //         console.log(rating);
 
-        //         var playerImage = $("<img>");
-        //         playerImage.attr("src", results[i].images.fixed_height_still.url).attr("data-state", "still").attr("data-still", results[i].images.fixed_height_still.url).attr("data-animate", results[i].images.fixed_height.url).attr("class", "gif img-fluid");
+            //         var p = $("<p>").text("Rating: " + rating);
 
-        //         gifDiv.prepend(p);
-        //         gifDiv.prepend(playerImage);
+            //         var playerImage = $("<img>");
+            //         playerImage.attr("src", results[i].images.fixed_height_still.url).attr("data-state", "still").attr("data-still", results[i].images.fixed_height_still.url).attr("data-animate", results[i].images.fixed_height.url).attr("class", "gif img-fluid");
 
-        //         $("#gifReturn").prepend(gifDiv);
+            //         gifDiv.prepend(p);
+            //         gifDiv.prepend(playerImage);
 
-                
+            //         $("#gifReturn").prepend(gifDiv);
+
+
         })
 
-    
 
 
 
-    });
+
+});
 
 
