72dd5c19f9ff6ad121bf65051f046c30e71e0364
Hrynchak <bohdanhrynchak@gmail.com>
2019-04-29 21:35:47 -0500

    228

--- Assets/js/flipcards.js
+++ Assets/js/flipcards.js
@@ -1,3 +1,9 @@
-function flip() {
-    $('.flip-card').toggleClass('flipped');
+$('.flip-card').click(function(){
+  $(this).find('.flip-card-inner').toggleClass('flipped');
+  if ($(this).find('.flip-card-inner').hasClass("flipped")){
+    // updateinfo
+    var beachId = $(this).attr("data-beach-id");
+    var beachName = $(this).attr("data-beach-name");
+    
   }
+});
