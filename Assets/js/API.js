197aa1e8990046d7a3fbf8f180f6426b34eb61c7
kpassaglia <kpassaglia@gmail.com>
2019-05-02 17:18:11 -0500

    clean up and comment adds

--- Assets/js/API.js
+++ Assets/js/API.js
@@ -1,13 +1,12 @@
 //Array of Beach Objects
-var beachIDs = [
-    {
+var beachIDs = [{
         dataBeachName: "Montrose",
         beach: "Montrose Beach",
         foodQuery: "restaurants%20near%20Montrose%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJlyqafN3TD4gRNJsjKC7Alk8",
         locationQuery: "41.9650322,-87.6347771",
-        latQuery:"",
-        lonQuery:""
+        latQuery: "",
+        lonQuery: ""
     },
     {
         dataBeachName: "OakStreet",
@@ -15,8 +14,8 @@ var beachIDs = [
         foodQuery: "restaurants%20near%20Oak%20Street%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJQZJQJFfTD4gRajRN4wWCges",
         locationQuery: "41.9019772,-87.6222749",
-        latQuery:"",
-        lonQuery:""
+        latQuery: "",
+        lonQuery: ""
     },
     {
         dataBeachName: "NorthAve",
@@ -24,18 +23,18 @@ var beachIDs = [
         foodQuery: "restaurants%20near%20North%20Avenue%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJDw9m7GbTD4gRfxvHEJFgLBs",
         locationQuery: "41.9141994,-87.6244975",
-        latQuery:"41.9141994",
-        lonQuery:"-87.6244975"
+        latQuery: "41.9141994",
+        lonQuery: "-87.6244975"
     },
-    
+
     {
         dataBeachName: "12thStreet",
         beach: "12th Street Beach",
         foodQuery: "restaurants%20near%2012th%20Street%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJIfbnQHArDogRFE5PEwDHQbU",
         locationQuery: "41.8636,-87.6074",
-        latQuery:"",
-        lonQuery:""
+        latQuery: "",
+        lonQuery: ""
     },
     {
         dataBeachName: "Ohio",
@@ -43,9 +42,9 @@ var beachIDs = [
         foodQuery: "restaurants%20near%20Ohio%20Street%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJwR5paVMrDogRhMIXATY07a4",
         locationQuery: "41.8935,-87.6129",
-        latQuery:"",
-        lonQuery:"",
-        counter:""
+        latQuery: "",
+        lonQuery: "",
+        counter: ""
     },
     {
         dataBeachName: "Osterman",
@@ -53,8 +52,8 @@ var beachIDs = [
         foodQuery: "restaurants%20near%20Hollywood%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJYwGn4HDRD4gRJ1_Oj6Oh-R8",
         locationQuery: "41.9862,-87.6520",
-        latQuery:"",
-        lonQuery:""
+        latQuery: "",
+        lonQuery: ""
     },
     {
         dataBeachName: "Foster",
@@ -62,8 +61,8 @@ var beachIDs = [
         foodQuery: "restaurants%20near%20Foster%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJt9K2mXzRD4gRn6RCpGid0n0",
         locationQuery: "41.9800,-87.6499",
-        latQuery:"",
-        lonQuery:""
+        latQuery: "",
+        lonQuery: ""
     },
     {
         dataBeachName: "63rdStreet",
@@ -71,54 +70,55 @@ var beachIDs = [
         foodQuery: "restaurants%20near%2063rd%20Street%20Beach%2C%20Chicago%2C%20Illinois%2C%20USA",
         beachQuery: "place_id:ChIJ1_rYG68pDogR0iUE_kgJ2cc",
         locationQuery: "41.7820,-87.5733",
-        latQuery:"",
-        lonQuery:""
+        latQuery: "",
+        lonQuery: ""
     }
 ];
 
-    // var map;
-    //     function initMap() {
-    //         map = new google.maps.Map(document.getElementById('map'), {
-    //             center: { lat: 41.9019772, lng: -87.6222749 },
-    //             zoom: 15
-    //         });
-    //     };
+// var map;
+//     function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//             center: { lat: 41.9019772, lng: -87.6222749 },
+//             zoom: 15
+//         });
+//     };
 
 
 $(".flip-card").on("click", function () {
-   
-    var currentBeach= $(this).attr("data-beach-name");
+
+    var currentBeach = $(this).attr("data-beach-name");
 
     //Loop through Array
     var values = Object.values(beachIDs);
     console.log(values);
-   
+
     //get lat lon for dark sky api
     for (i = 0; i < values.length; i++) {
         if (values[i].dataBeachName == currentBeach) {
-          
+
             var locationQueryString = values[i].locationQuery;
-           console.log(locationQueryString);
+            console.log(locationQueryString);
+            var queryURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4a4040a2a6849005fa4c1146c74f1d8f/" + locationQueryString;
             //Dark Sky API Call with Selected Beach
-           
+
             $.ajax({
-               url: "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4a4040a2a6849005fa4c1146c74f1d8f/" + locationQueryString,
-                method: "GET",
-            })
+                    url: queryURL,
+                    method: "GET",
+                })
                 .then(function (response) {
                     console.log(response);
-                   var results = response;
-                   console.log("test" + results.currently.apparentTemperature)
-                   $("#weather"+currentBeach).text(results.currently.apparentTemperature);
+                    var results = response;
+                    console.log("test" + results.currently.apparentTemperature)
+                    $("#weather" + currentBeach).text(results.currently.apparentTemperature);
                 })
-            
+
         } else {
             return
         }
     }
 });
 
-    // var map;
+// var map;
 //         function initMap() {
 //             map = new google.maps.Map(document.getElementById('map'), {
 //                 center: { lat: 41.9019772, lng: -87.6222749 },
